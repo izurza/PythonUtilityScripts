fabfe5649a89b9fe0e4d13a26f912b50b41affdc
izurza <100201660+izurza@users.noreply.github.com>
2022-10-17 08:53:47 +0200

    Add files via upload

---
+++ emf.py
@@ -0,0 +1,15 @@
+import glob
+import email
+
+if __name__ == '__main__':
+    files = glob.glob("/Users/izurz/Desktop/phishing9.eml")
+    for each in files:
+        msg = email.message_from_file(open(each))
+        attachments = msg.get_payload()
+        for attachment in attachments:
+            try:
+                fnam = attachment.get_filename()
+                f = open(fnam, 'wb').write(attachment.get_payload(decode=True,))
+                f.close()
+            except Exception as detail:
+                pass
